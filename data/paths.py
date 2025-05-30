c45e2094526b7d590e8d0fcdb7511fee028b91bc
Imaanshaik <115191377+Imaanshaik@users.noreply.github.com>
2025-05-30 20:00:50 +0200

    Create paths.py

---
+++ data/paths.py
@@ -0,0 +1,9 @@
+import sys
+from pathlib import Path
+
+MAIN_DIR = Path(sys.argv[0]).resolve().parent   
+MOTION_SENSE_PATH = MAIN_DIR.parent / "motion-sense"  
+SUBJECTS_INFO_CSV_PATH = MOTION_SENSE_PATH / "data" / "data_subjects_info.csv"
+A_DEVICE_MOTION_DATA_PATH = MOTION_SENSE_PATH / "data" / "A_DeviceMotion_data"
+B_DEVICE_MOTION_DATA_PATH = MOTION_SENSE_PATH / "data" / "B_DeviceMotion_data"
+C_DEVICE_MOTION_DATA_PATH = MOTION_SENSE_PATH / "data" / "C_DeviceMotion_data"
